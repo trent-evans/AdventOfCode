7f9e35209d4572a80cac2b614c0cdaeeba289094
trent-evans <tlevans1116@gmail.com>
2021-12-13 11:24:40 -0700

    Getting started on day 5

--- 2021/day5/main.go
+++ 2021/day5/main.go
@@ -4,10 +4,11 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type indexPair struct {
+type ventLine struct {
 	x1 int
 	y1 int
 	x2 int
@@ -17,14 +18,6 @@ type indexPair struct {
 func main() {
 
 	filename := "2021/day5/example.txt"
-
-	side := 1000 // For the full size
-
-	if strings.Contains(filename, "example") {
-		side = 10
-	}
-
-	var thermalField [side][side]int
 
 	file, err := os.Open("2021/day5/example.txt")
 
@@ -36,7 +29,43 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
+	var ventList []ventLine
 
+	for scanner.Scan() {
+		entry := scanner.Text()
+		entry = strings.Replace(entry, "->", " ", -1)
+		entry = strings.Replace(entry, ",", " ", -1)
+
+		valuesSplit := strings.Fields(entry)
+
+		var newLine ventLine
+		for idx, indVal := range valuesSplit {
+
+			num, _ := strconv.Atoi(indVal)
+
+			switch idx {
+
+			case 0:
+				newLine.x1 = num
+
+			case 1:
+				newLine.y1 = num
+
+			case 2:
+				newLine.x2 = num
+
+			case 3:
+				newLine.y2 = num
+			}
+		}
+
+		ventList = append(ventList, newLine)
+	}
+
+	bound := 1000 // For the full size
+	var thermalField [1000][1000]int
+
+	if strings.Contains(filename, "example") {
+		bound = 10
 	}
 }
